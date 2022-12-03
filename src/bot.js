50be16cf9d985bf808a2507a4f3d94ed2c738fc3
Jordan Masone <jordanmasone1@gmail.com>
2022-12-03 13:20:37 -0500

    Added message listener.

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,29 @@
 require("dotenv").config();
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 
 // Create client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
+// Ready Listener
+client.on("ready", () => {
+    console.log("Code Blue Bot is Online!");
+
+    client.user.setActivity("I'm watching you Wazowski, always watching.", {type: "WATCHING"})
 });
 
+// Message Listener
+client.on("messageCreate", message => {
+    try {
+        if (message.author.bot) return;
+
+        // message.channel.send("blah blah blah blah blah blah blah");
+        message.reply("blah blah blah blah blah blah blah");
+
+        console.log("Replied to message creation.")
+    } catch (err) {
+        console.error(err)
+    }
+});
+
+// Login and Turn on Bot
 client.login(process.env.DISCORD_BOT_TOKEN);
